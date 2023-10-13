8713981562070392cddf0e5d56bc855c775d34e6
JPruezkiez1 <jpruezkiez@icloud.com>
2023-10-13 14:12:08 -0500

    first

---
+++ App.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+app.use(fileUpload());
+
+app.post('/upload', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    const image = req.files.image;
+    image.mv('/home/paulamar9428/images/' + image.name, (err) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        res.send('File uploaded!');
+    });
+});
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
