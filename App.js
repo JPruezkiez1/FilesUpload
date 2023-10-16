d4e31279637a2475fcb3e1c089877d301da9c79b
JPruezkiez1 <jpruezkiez@icloud.com>
2023-10-16 17:52:02 -0500

    resolve port

--- App.js
+++ App.js
@@ -59,6 +59,6 @@ app.post('/upload', (req, res) => {
     });
 });
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8000');
+app.listen(3000, () => {
+    console.log('Server is running on port');
 });
